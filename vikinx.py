35c992462af24926a74773647a286b7b45a5a965
rrah <redsword_99@hotmail.co.uk>
2014-07-25 20:30:09 +0100

    might work now

--- vikinx.py
+++ vikinx.py
@@ -1,4 +1,5 @@
 import telnet as tel
+from binascii import hexlify as binhex
 
 host = 'localhost'
 port = 2001
@@ -6,12 +7,20 @@ port = 2001
 class vikinx(tel.Telnet):
     
     def update(self):
-##        self.open()
-        self.write(bytearray([1,1,0,0,0,0,0,0]))
-        self.write(bytearray([1,1,0,0,0,0,0,0]))
         
-    def self.write(self, bytes):
-        print bytes
+        """
+        Get routing information and update object connections"""
+        
+        self.open()
+        self.write(bytearray.fromhex('c000'))
+        output = binhex(self.read_until('c000'))
+        self.close()
+        
+        output = output.split['a0']
+        for out in output:
+            out = [out[0:2], out[2, 4]]
+        print output
+        
         
 vik = vikinx()
 vik.update
