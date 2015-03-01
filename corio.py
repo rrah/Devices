feecae999157ef0a0b6764f8feafc5bea6e4dc15
Robert Walker <rrah99@gmail.com>
2015-03-01 22:32:29 +0000

    Better handling of everything

--- corio.py
+++ corio.py
@@ -11,18 +11,34 @@
 
 import telnet as tel
 
-from time import sleep
+import device
 
-class Corio(tel.Telnet):
+from socket import timeout
 
-    def fadeSpeed(self, speed):
+TIMEOUT = 0.5
+
+class Corio(tel.Telnet, device.Device):
+
+    def set_fade_speed(self, speed):
 
         """
         int speed = 1 - 25 set speed of fades"""
-
-        self.open()
-        self.write('fade speed={}\r\n'.format(speed))
-        self.close()
+        
+        with self:
+            self.open()
+            self.write('fade speed={}\r\n'.format(speed))
+            try:
+                self.read_until('>', TIMEOUT)
+            except timeout:
+                self.close()
+                return False
+            self.close()
+            self._fade_speed = speed
+            return True
+        
+    def get_fade_speed(self):
+        
+        return self._fade_speed
 
     def fade(self, in_):
 
@@ -30,25 +46,51 @@ class Corio(tel.Telnet):
         fade the overlay in or out
 
         bool in_ = true for in, false for out"""
-
-        self.open()
-        if in_:
-            self.write('fade=1\r\n')
-        else:
-            self.write('fade=0\r\n')
-        self.close()
+        with self:
+            self.open()
+            if in_:
+                self.write('fade=1\r\n')
+            else:
+                self.write('fade=0\r\n')
+            read = self.expect(['[>]', '[?]'], TIMEOUT)
+            if read[0]:
+                self.close()
+                return False
+            self.close()
+            return True
+        
+    def is_faded_in(self):
+        
+        return self._faded_in
+    
+    def update(self):
+        
+        with self:
+            self.open()
+            self.write('fade\r\n')
+            read = (self.expect(['[0-1]'], 1))
+            self._faded_in = True if '1' in read[2] else False if '0' in read[2] else None
+            self.write('fade speed\r\n')
+            self._fade_speed = int(self.expect(['[0-9]{1,2}'], 1)[2])
+            self.close()
+        return True
+        
 
     def __init__(self, host, port, *args, **kwargs):
         tel.Telnet.__init__(self, host, port, *args, **kwargs)
-
+        
+    def fade_in(self):
+        
+        return self.fade(True)
+        
+    def fade_out(self):
+        
+        return self.fade(False)
 
 
 def main():
-    cor = Corio('localhost', 2000)
-    cor.fadeSpeed(5)
-    cor.fade(True)
-    sleep(10)
-    cor.fade(False)
+    cor = Corio('localhost', 2003)
+    print cor.get_fade_speed()
 
 if __name__ == '__main__':
     main()
